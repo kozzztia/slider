31e3037144d9c7d0699eb493af9019cd3f008354
kozzztia <kozzztia@gmail.com>
2025-02-27 02:16:58 +0200

    slider position

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.time
+
+
+function createSlider() {
+    const slider = document.querySelector('.slider');
+    const slides = document.querySelectorAll('.slide');
+    console.log(slider);
+}
+
+createSlider();
