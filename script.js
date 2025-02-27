525b631e62102429ca7c8da0b02a1dad0c6ad22e
kozzztia <kozzztia@gmail.com>
2025-02-27 09:17:14 +0200

    buttons navigation

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 
+function createNavigation () {
+    const navigation = document.createElement('div');
+    navigation.classList.add('navigation');
+
+    const nextBtn = document.createElement('button');
+    nextBtn.classList.add(['button']);
+    nextBtn.innerHTML = '<i class="fas fa-arrow-right"></i>';
+    navigation.appendChild(nextBtn);
+
+    const prevBtn = document.createElement('button');
+    prevBtn.classList.add(['button']);
+
+    prevBtn.innerHTML = '<i class="fas fa-arrow-left"></i>';
+    navigation.appendChild(prevBtn);
+
+    return navigation
+}
+
 
 function createSlider() {
     const slider = document.querySelector('.slider');
     const slides = document.querySelectorAll('.slide');
-    console.log(slider);
+
+    
+    const navigationButtons = createNavigation()
+    
+    slider.append(navigationButtons)
 }
 
 createSlider();
